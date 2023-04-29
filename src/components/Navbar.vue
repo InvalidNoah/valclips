e04cb48216407883c2054ffeaf5e8195903cc2f9
brokiem <milyhqvideo@gmail.com>
2023-04-29 16:33:35 +0800

    Better add clip button style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,8 @@
         </div>
 
         <div class="flex items-center mr-8">
-            <div @click="openSubmitForm" class="flex items-center hover:cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div @click="openSubmitForm" class="flex items-center bg-[#272727] hover:bg-[#202020] rounded-md p-1.5 hover:cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
                 </svg>
             </div>
