9f4fc4b98bb2aa5cd68f466fde71afd7f9a75e41
brokiem <milyhqvideo@gmail.com>
2023-05-01 17:47:48 +0800

    fix: page speed insight render blocking resources

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import './assets/css/font.css'
+import './assets/css/style.css'
 import App from './App.vue'
 import router from "./router/router.ts";
 
